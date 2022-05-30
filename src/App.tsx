6427d33aa8588215a2cde0a13de0b9dbe68164d9
kazuma.suzuki <ka-suzuki@cresco.co.jp>
2022-05-30 18:48:25 +0900

    大吉30個ほしい

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,15 @@
 import { Box, Container } from '@mui/material';
 import React from 'react';
+import {range} from "lodash";
 import Omikuji from './Components/Omikuji';
 
 function App() {
+  // 大吉30個ほしい
+  const daikichiCount = 30;
   return (
     <Container>
       <Box sx={{ display: 'flex', flexWrap: 'wrap' }}>
-        <Omikuji>大吉</Omikuji>
+        {range(daikichiCount).map(_=><Omikuji>大吉</Omikuji>)}
         <Omikuji>中吉</Omikuji>
         <Omikuji>小吉</Omikuji>
       </Box>
